3ecac259ecda95b11d48d11587c4d6730ff56e9b
elisardo <elisalma@myuax.com>
2024-01-20 22:10:33 +0100

    solucion

---
+++ src/LinkedList.java
@@ -0,0 +1,16 @@
+package src;
+
+public class LinkedList<T> {
+    public T size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+
+    public void addLast(T numero) {
+    }
+
+    public T removeFirst() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+}
---
+++ src/Utilidades.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Utilidades {
+    public static int numAzar(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    public static void esperarTiempoAzar(int i) {
+    }
+}
